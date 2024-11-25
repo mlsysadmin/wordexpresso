64535e03dccdeb3c745098a5a4b0e55749c16b7e
Kate Sheina Ceballos <kate.ceballos@mlhuillier.com>
2024-11-25 09:30:41 +0800

    Normalize line endings

--- frontend/wp=config.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once(dirname(__FILE__) . '/config/application.php');
-
-if (!defined('ABSPATH')) {
-    define('ABSPATH', dirname(__FILE__) . '/wp/');
-}
-
-require_once(ABSPATH . 'wp-settings.php');
